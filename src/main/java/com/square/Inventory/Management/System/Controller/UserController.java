3723a369cebea3ed08152ef1b9b525ab42b98fd9
moeen775 <1821775@iub.edu.bd>
2022-12-28 15:55:53 +0600

    --pagination --update user -- delete user

--- src/main/java/com/square/Inventory/Management/System/Controller/UserController.java
+++ src/main/java/com/square/Inventory/Management/System/Controller/UserController.java
@@ -1,12 +1,11 @@
 package com.square.Inventory.Management.System.Controller;
 
+import com.square.Inventory.Management.System.Entity.User;
 import org.springframework.core.io.Resource;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Map;
 
 @RequestMapping(path = "/inventory/user")
@@ -20,5 +19,24 @@ public interface UserController {
     @GetMapping(path = "/download/userAsExcel/")
     ResponseEntity<Resource> getFile();
 
+    @PutMapping(path = "/update/{userId}")
+    ResponseEntity<User> updateUser(@RequestBody User user,@PathVariable("userId") Integer userId);
 
+    @DeleteMapping(path = "/delete/{userId}")
+    ResponseEntity<String> deleteUser(@PathVariable("userId") Integer userId);
+
+    @GetMapping(path = "/get/all")
+    ResponseEntity<List<User>> getAllUser();
+
+    @GetMapping(path = "/get/all/page")
+    ResponseEntity<?> getAllByPagination(@RequestParam(defaultValue = "0") int page,
+                                         @RequestParam(defaultValue = "3") int size);
+
+    @GetMapping(path = "/get/all/pageSort")
+    ResponseEntity<?> getAllByPaginationBySorting(@RequestParam(defaultValue = "0") int page,
+                                                  @RequestParam(defaultValue = "3") int size,
+                                                  @RequestParam(defaultValue = "id") String  sortBy);
 }
+
+
+
--- src/main/java/com/square/Inventory/Management/System/ControllerImpl/UserControllerImpl.java
+++ src/main/java/com/square/Inventory/Management/System/ControllerImpl/UserControllerImpl.java
@@ -2,6 +2,7 @@ package com.square.Inventory.Management.System.ControllerImpl;
 
 import com.square.Inventory.Management.System.Constant.InventoryConstant;
 import com.square.Inventory.Management.System.Controller.UserController;
+import com.square.Inventory.Management.System.Entity.User;
 import com.square.Inventory.Management.System.IMSUtils.InventoryUtils;
 import com.square.Inventory.Management.System.JWT.JWTFilter;
 import com.square.Inventory.Management.System.Service.UserService;
@@ -14,6 +15,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -55,4 +57,55 @@ public class UserControllerImpl implements UserController {
                 .body(file);
 
     }
+
+    @Override
+    public ResponseEntity<User> updateUser(User user, Integer userId) {
+        return userService.update(user,userId);
+    }
+
+    @Override
+    public ResponseEntity<String> deleteUser(Integer userId)
+    {
+        if(jwtFilter.isAdmin()){
+            return userService.deleteUser(userId);
+        }else {
+            return InventoryUtils.getResponse(InventoryConstant.UNAUTHORIZED_ACCESS,HttpStatus.UNAUTHORIZED);
+        }
+    }
+
+    @Override
+    public ResponseEntity<List<User>> getAllUser() {
+        if(jwtFilter.isAdmin()){
+            return new ResponseEntity<List<User>>(userService.getAllUser(), HttpStatus.OK);
+        }else {
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
+
+    }
+
+    @Override
+    public ResponseEntity<List<User>> getAllByPagination(int page, int size) {
+        if(jwtFilter.isAdmin())
+        {
+            List<User> userList=userService.getAllUserByPagination(page,size);
+            return new ResponseEntity<List<User>>(userList,HttpStatus.OK);
+        }else {
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+
+        }
+    }
+
+    @Override
+    public ResponseEntity<List<User>> getAllByPaginationBySorting(int page, int size, String sortBy) {
+        if(jwtFilter.isAdmin())
+        {
+            List<User> userList=userService.getAllUserByPaginationBySort(page,size,sortBy);
+            return new ResponseEntity<List<User>>(userList,HttpStatus.OK);
+        }else {
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+
+        }
+
+    }
+
 }
