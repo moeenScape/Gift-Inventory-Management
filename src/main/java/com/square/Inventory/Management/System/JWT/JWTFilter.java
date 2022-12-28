26a46ca69a99678f850b28c057df84a1494ebea8
moeen775 <1821775@iub.edu.bd>
2022-12-28 13:18:55 +0600

    -

--- src/main/java/com/square/Inventory/Management/System/JWT/JWTFilter.java
+++ src/main/java/com/square/Inventory/Management/System/JWT/JWTFilter.java
@@ -6,6 +6,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
@@ -14,6 +15,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@Component
 public class JWTFilter extends OncePerRequestFilter {
 
     @Autowired
