af133f70ca82df5a4eaccc35dc7aa38c8573ed29
moeen775 <1821775@iub.edu.bd>
2022-12-28 17:20:32 +0600

    --query for DSO,DPG,DU,Depot --query for Summery

--- src/main/java/com/square/Inventory/Management/System/Controller/BudgetController.java
+++ src/main/java/com/square/Inventory/Management/System/Controller/BudgetController.java
@@ -1,9 +1,13 @@
 package com.square.Inventory.Management.System.Controller;
 
+import com.square.Inventory.Management.System.DTO.BudgetSummary;
+import com.square.Inventory.Management.System.DTO.DEPOT;
+import com.square.Inventory.Management.System.DTO.SSU;
 import com.square.Inventory.Management.System.ExcelHepler.BudgetDTO;
 import com.square.Inventory.Management.System.Entity.Budget;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -16,4 +20,21 @@ public interface BudgetController {
 
     @PostMapping("/addBudgetFromExcel")
     public ResponseEntity<List<Budget>> addBudgetDTOFromExcel();
+
+    @GetMapping(path = "/budget/for/ssu/{ssuName}")
+    ResponseEntity<List<SSU>> getBudgetBySSU(@PathVariable String ssuName);
+
+    @GetMapping(path = "/budget/for/depot/{id}")
+    ResponseEntity<List<DEPOT>> getBudgetByDepotID(@PathVariable String id);
+
+    @GetMapping(path = "/budget/by/{month}")
+    ResponseEntity<Budget> viewAllBudgetByMonth(@PathVariable("month") String month);
+
+    @GetMapping(path = "/budget/all")
+    ResponseEntity<List<Budget>> getAllBudget();
+
+    @GetMapping(path = "/summary")
+    ResponseEntity<List<BudgetSummary>>  getSummary();
+
+
 }
--- src/main/java/com/square/Inventory/Management/System/Service/BudgetService.java
+++ src/main/java/com/square/Inventory/Management/System/Service/BudgetService.java
@@ -1,8 +1,13 @@
 package com.square.Inventory.Management.System.Service;
 
 
+import com.square.Inventory.Management.System.DTO.BudgetSummary;
+import com.square.Inventory.Management.System.DTO.DEPOT;
+import com.square.Inventory.Management.System.DTO.SSU;
 import com.square.Inventory.Management.System.Entity.Budget;
 import com.square.Inventory.Management.System.ExcelHepler.BudgetDTO;
+import org.springframework.http.ResponseEntity;
+
 import java.util.List;
 
 public interface BudgetService {
@@ -10,5 +15,15 @@ public interface BudgetService {
     List<BudgetDTO> getAllBudgetFromExcel();
 
     List<Budget> addBudgetFromExcel();
+
+    ResponseEntity<List<SSU>> getBudgetForSSUByName(String ssuName);
+
+    ResponseEntity<List<DEPOT>> getBudgetForDepotByID(String depotID);
+
+    ResponseEntity<Budget> viewAllBudgetByMonth(String month);
+
+    ResponseEntity<List<Budget>> getAllBudget();
+
+    ResponseEntity<List<BudgetSummary>> getSummary();
 
 }
