d2f3c41645c820315b264654fc29c22fad04bc3a
Safat99 <sarkersafat99@gmail.com>
2023-01-01 11:34:36 +0600

    missed depotRepository to the prev commit

--- src/main/java/com/square/Inventory/Management/System/Repository/DepotRepository.java
+++ src/main/java/com/square/Inventory/Management/System/Repository/DepotRepository.java
@@ -1,9 +1,16 @@
 package com.square.Inventory.Management.System.Repository;
 
 import com.square.Inventory.Management.System.Entity.Depot;
+import com.square.Inventory.Management.System.Projection.DepotProjectionInterface;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface DepotRepository extends JpaRepository<Depot,Long> {
+
+    @Query(value = "select depot_name,location from Depot", nativeQuery = true)
+    List<DepotProjectionInterface> getAllDepotName();
 }
