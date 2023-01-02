dfad3b90abbdda5dfe90fa2fe380ecafcef2d162
moeen775 <1821775@iub.edu.bd>
2023-01-02 17:56:46 +0600

    -update SecurityConfig

--- src/main/java/com/square/Inventory/Management/System/JWT/JWTFilter.java
+++ src/main/java/com/square/Inventory/Management/System/JWT/JWTFilter.java
@@ -35,7 +35,7 @@ public class JWTFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest httpServletRequest,
                                     HttpServletResponse httpServletResponse,
                                     FilterChain filterChain) throws ServletException, IOException {
-        if (httpServletRequest.getServletPath().matches("/inventory/user/login")) {
+        if (httpServletRequest.getServletPath().matches("/user/login")) {
             filterChain.doFilter(httpServletRequest, httpServletResponse);
         } else {
             String authorizationHeader = httpServletRequest.getHeader("Authorization");
