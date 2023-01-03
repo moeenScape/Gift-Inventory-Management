57c54768723a482e71070d88f96d819a7aeda3b8
moeen775 <1821775@iub.edu.bd>
2023-01-03 13:19:32 +0600

    -sql

--- src/main/resources/migration_file/user.sql
+++ src/main/resources/migration_file/user.sql
@@ -1,18 +1,17 @@
 --create Table for User
 create table user(
-userId int Not NULL,
-firstName varchar(250),
-lastName varchar(250),
+userid int Not NULL,
+first_name varchar(250),
+last_name varchar(250),
 email varchar(250),
-contractNumber varchar(11),
+contract_number varchar(11),
 password varchar(10),
 role varchar(10),
 status varchar(10),
-userDescription varchar(250),
-workPlace varchar(10),
+
 CONSTRAINT user_pk PRIMARY KEY (userId)
 );
 
 --insert
-insert into user(userId,firstName,lastName,email,contractNumber,password,role,status,userDescription,workPlace)
-value(?,?,?,?,?,?,?,?,?,?);
+insert into user(userid,first_name,last_name,email,contract_number,password,role,status)
+value(?,?,?,?,?,?,?,?,);
