60a63fc4c8dba3399225fee6a749a4c1790c7b3d
moeen775 <1821775@iub.edu.bd>
2023-01-04 16:17:51 +0600

    --add ssu

---
+++ src/main/java/com/square/Inventory/Management/System/Repository/SampleSectionRepository.java
@@ -0,0 +1,7 @@
+package com.square.Inventory.Management.System.Repository;
+
+import com.square.Inventory.Management.System.Entity.SampleSectionUnit;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SampleSectionRepository extends JpaRepository<SampleSectionUnit,Integer> {
+}
