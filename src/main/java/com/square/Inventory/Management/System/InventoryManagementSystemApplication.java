bc2efddc2d15d9b281ca8e6c9cf1b85ae600da24
moeen775 <1821775@iub.edu.bd>
2023-01-10 18:21:30 +0600

    -CROS disable for Angular

--- src/main/java/com/square/Inventory/Management/System/InventoryManagementSystemApplication.java
+++ src/main/java/com/square/Inventory/Management/System/InventoryManagementSystemApplication.java
@@ -12,14 +12,6 @@ public class InventoryManagementSystemApplication {
     public static void main(String[] args) {
         SpringApplication.run(InventoryManagementSystemApplication.class, args);
     }
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("http://localhost:4200");
-            }
-        };
-    }
+
 }
 
