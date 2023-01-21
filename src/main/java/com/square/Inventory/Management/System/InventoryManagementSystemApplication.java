0d5c1ff6b2e0ce337560db63b7416927cf30f766
moeen775 <1821775@iub.edu.bd>
2023-01-21 17:53:34 +0600

    -global cors Config(1)

--- src/main/java/com/square/Inventory/Management/System/InventoryManagementSystemApplication.java
+++ src/main/java/com/square/Inventory/Management/System/InventoryManagementSystemApplication.java
@@ -2,6 +2,12 @@ package com.square.Inventory.Management.System;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 @SpringBootApplication
 public class InventoryManagementSystemApplication {
 
@@ -9,6 +15,21 @@ public class InventoryManagementSystemApplication {
         SpringApplication.run(InventoryManagementSystemApplication.class, args);
     }
 
+    @Bean
+    public WebMvcConfigurer coresConfigure() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*")
+                        .allowCredentials(true)
+                        .maxAge(3600);
+            }
+        };
+
+    }
 
 }
 
