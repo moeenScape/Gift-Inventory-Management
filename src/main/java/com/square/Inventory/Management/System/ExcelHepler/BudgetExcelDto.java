0e3d8a58e314487e167e5fe2421737bf819aeb2d
Safat99 <sarkersafat99@gmail.com>
2023-01-21 16:34:02 +0600

    dto validation added and getter used

--- src/main/java/com/square/Inventory/Management/System/ExcelHepler/BudgetExcelDto.java
+++ src/main/java/com/square/Inventory/Management/System/ExcelHepler/BudgetExcelDto.java
@@ -3,17 +3,26 @@ package com.square.Inventory.Management.System.ExcelHepler;
 import com.square.Inventory.Management.System.Entity.SBU;
 import com.poiji.annotation.ExcelCell;
 import com.poiji.annotation.ExcelRow;
+import lombok.Getter;
 
+import javax.validation.constraints.Pattern;
+
+@Getter
 public class BudgetExcelDto {
+    // todo naming change after FE approval
+
     @ExcelRow
     private int rowIndex;
+
     @ExcelCell(0)
     private String budgetID;
+
     @ExcelCell(1)
     private String sapCode;
 
     @ExcelCell(2)
     private String productName;
+
     @ExcelCell(3)
     private String productionUnit;
 
@@ -30,7 +39,7 @@ public class BudgetExcelDto {
     private String fieldColleagueName;
 
     @ExcelCell(8)
-    private int quantity;
+    private float quantity;
 
     @ExcelCell(9)
     private String depotName;
@@ -38,6 +47,7 @@ public class BudgetExcelDto {
     @ExcelCell(10)
     private String depotID;
 
+    @Pattern(regexp = "^(ppm|sample|gift|PPM|SAMPLE|GIFT|Ppm|Sample|Gift)$", message = "unknown category inserted")
     @ExcelCell(11)
     private String category;
 
@@ -49,65 +59,5 @@ public class BudgetExcelDto {
 
     @ExcelCell(14)
     private String ssu_id;
-
-    public String getBudgetID() {
-        return budgetID;
-    }
-
-    public String getSapCode() {
-        return sapCode;
-    }
-
-    public String getProductName() {
-        return productName;
-    }
-
-    public String getProductionUnit() {
-        return productionUnit;
-    }
-
-    public String getPackageSize() {
-        return packageSize;
-    }
-
-    public SBU getSbu() {
-        return sbu;
-    }
-
-    public String getFieldColleagueID() {
-        return fieldColleagueID;
-    }
-
-    public int getQuantity() {
-        return quantity;
-    }
-
-    public String getDepotName() {
-        return depotName;
-    }
-
-    public String getDepotID() {
-        return depotID;
-    }
-
-    public String getCategory() {
-        return category;
-    }
-
-    public String getMonth() {
-        return month;
-    }
-
-    public int getYear() {
-        return year;
-    }
-
-    public String getSsu_id() {
-        return ssu_id;
-    }
-
-    public String getFieldColleagueName() {
-        return fieldColleagueName;
-    }
 
 }
